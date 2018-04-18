7f07f2c063f28d4b51a9acc299c38882ba313f20
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-18 14:53:42 +0100

    Maximum temperature with tests

--- spec/ThermoSpec.js
+++ spec/ThermoSpec.js
@@ -44,5 +44,14 @@ describe('Thermostat', function() {
     expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
 
+  describe('when power saving mode is on', function() {
+  it('has a maximum temperature of 25 degrees', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
+  });
+});
+
 
 });
--- src/Thermo.js
+++ src/Thermo.js
@@ -4,6 +4,8 @@ function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
   this.powerSavingMode = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
 };
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -11,6 +13,9 @@ Thermostat.prototype.getCurrentTemperature = function() {
 };
 
 Thermostat.prototype.up = function() {
+  if (this.isMaximumTemperature()) {
+    return;
+  }
   this.temperature += 1;
 };
 
@@ -35,4 +40,11 @@ Thermostat.prototype.switchPowerSavingModeOff = function() {
 
 Thermostat.prototype.switchPowerSavingModeOn = function() {
   this.powerSavingMode = true;
+};
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false) {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
 }
