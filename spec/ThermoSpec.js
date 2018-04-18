4c8d9069904d461a1de01cf92e5ea18d4864a770
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-18 13:57:18 +0100

    implemented minimum temperature with test

--- spec/ThermoSpec.js
+++ spec/ThermoSpec.js
@@ -10,12 +10,23 @@ describe('Thermostat', function() {
   it('starts at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
   it('increases in temperature with up()', function() {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
+
   it('decreases in temperature with down()', function() {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+
 });
