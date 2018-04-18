19df9bdbe15babdfb15fb041e02c1d7c5985732a
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-18 13:33:44 +0100

    refactor first test

--- spec/ThermoSpec.js
+++ spec/ThermoSpec.js
@@ -8,6 +8,6 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 });
--- src/Thermo.js
+++ src/Thermo.js
@@ -3,3 +3,7 @@
 function Thermostat() {
   this.temperature = 20;
 };
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature
+};
