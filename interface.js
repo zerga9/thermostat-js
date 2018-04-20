49a4aab1b480abd7e726ae2d1bc4ef13cd8c46f5
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-20 10:50:55 +0100

    css file addedfor change in color

--- interface.js
+++ interface.js
@@ -29,15 +29,7 @@ $('#powersaving-off').on('click', function() { // event listener
 
 function updateTemperature() {
   $('#temperature').text(thermostat.temperature);
-
-  if(thermostat.energyUsage() === 'low-usage') {
-    $('#temperature').css("color", "green")
-  }else if(thermostat.energyUsage() === 'medium-usage') {
-    $('#temperature').css("color", "black")
-
-    }else{
-      $("#temperature").css("color", "red")
-    }
+  $('#temperature').attr('class', thermostat.energyUsage());
 
     };
 
