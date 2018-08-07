db26d3753a1314b4499bd1d7b901169391513b1e
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-07 12:46:32 +0100

    Change erb in index and background image, styling  added

--- interface.js
+++ interface.js
@@ -2,59 +2,61 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $('#temperature-up').on('click', function() { // event listener
-  thermostat.up(); // update model
-  updateTemperature(); // update view
-});
-
-$('#temperature-down').on('click', function() { // event listener
-  thermostat.down(); // update model
-  updateTemperature(); // update view
-})
-
-$('#temperature-reset').on('click', function() { // event listener
-  thermostat.resetTemperature(); // update model
-  updateTemperature();
-});
-$('#powersaving-on').on('click', function() { // event listener
-  thermostat.switchPowerSavingModeOn(); // update model
-  $('#power-saving-status').text('on')
-  updateTemperature();
-});
-$('#powersaving-off').on('click', function() { // event listener
-  thermostat.switchPowerSavingModeOff(); // update model
-  $('#power-saving-status').text('off')
-  updateTemperature();
-});
-
-function updateTemperature() {
-  $('#temperature').text(thermostat.temperature);
-  $('#temperature').attr('class', thermostat.energyUsage());
-
-    };
-
-    function displayWeather(city) {
-      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-      var token = '&appid=19b8d89c9764def9aa48a6716ef909ce';
-      var units = '&units=metric';
-      $.get(url + token + units, function(data) {
-        $('#current-temperature').text(data.main.temp);
-      })
-    }
-
-    displayWeather('London')
-    $('#select-city').submit(function(event) {
-      event.preventDefault();
-      var city = $('#current-city').val();
-      $('#chosen-city').text(city);
-      displayWeather(city);
+  $("#temperature-up").on("click", function() {
+    // event listener
+    thermostat.up(); // update model
+    updateTemperature(); // update view
   });
-function displayTime() {
-  response = $.get("http://localhost:4567")
-  $.get(response, function(response) {
-    $('#time').text(response);
 
-})
-}
+  $("#temperature-down").on("click", function() {
+    // event listener
+    thermostat.down(); // update model
+    updateTemperature(); // update view
+  });
 
+  $("#temperature-reset").on("click", function() {
+    // event listener
+    thermostat.resetTemperature(); // update model
+    updateTemperature();
+  });
+  $("#powersaving-on").on("click", function() {
+    // event listener
+    thermostat.switchPowerSavingModeOn(); // update model
+    $("#power-saving-status").text("on");
+    updateTemperature();
+  });
+  $("#powersaving-off").on("click", function() {
+    // event listener
+    thermostat.switchPowerSavingModeOff(); // update model
+    $("#power-saving-status").text("off");
+    updateTemperature();
+  });
+
+  function updateTemperature() {
+    $("#temperature").text(thermostat.temperature);
+    $("#temperature").attr("class", thermostat.energyUsage());
+  }
+
+  function displayWeather(city) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
+    var token = "&appid=19b8d89c9764def9aa48a6716ef909ce";
+    var units = "&units=metric";
+    $.get(url + token + units, function(data) {
+      $("#current-temperature").text(data.main.temp);
+    });
+  }
+
+  displayWeather("London");
+  $("#select-city").submit(function(event) {
+    event.preventDefault();
+    var city = $("#current-city").val();
+    $("#chosen-city").text(city);
+    displayWeather(city);
+  });
+  function displayTime() {
+    response = $.get("http://localhost:9000");
+    $.get(response, function(response) {
+      $("#time").text(response);
+    });
+  }
 });
--- server.rb
+++
@@ -1,8 +0,0 @@
-require 'sinatra'
-# require 'json'
-
-set :public_folder, proc { File.join(root) }
-
-get '/' do
-  erb :index
-end
